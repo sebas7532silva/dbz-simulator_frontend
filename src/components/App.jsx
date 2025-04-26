6eb188df9e3c15b48f4e4d12368be633b25d4929
Freddy Silva <sebassilva7532@hotmail.com>
2025-04-26 12:26:06 -0600

    Adding the API to get all characters in setCharacters

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,31 @@
 import { useState } from 'react';
 import Header from './Header/Header';
+import api from '../utils/api.js';
+import { useEffect } from 'react';
 
 
 function App() {
     const [count, setCount] = useState(0)
+    const [characters, setCharacters] = useState([])
+    const [planets, setPlanets] = useState([])
+
+    // Get all planets and characters when mounting the component
+    useEffect(() => {
+        api.getPlanets()
+            .then((res) => {
+                setPlanets(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        api.getCharacters()
+            .then((res) => {
+                setCharacters(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
     return (
         <div className="page">
