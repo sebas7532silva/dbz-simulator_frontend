2c4c81e71fec766708598434b6ba30dd6576fe27
Freddy Silva <sebassilva7532@hotmail.com>
2025-05-01 14:48:28 -0600

    adding the summon container, working on functionality

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,16 +4,28 @@ import { useState } from "react";
 
 function Main({ cards, planets }) {
   const [userCards, setUserCards] = useState([]);
+  const [isCarruselOpen, setIsCarruselOpen] = useState(false);
+
+  function handleAddCards() {
+    setIsCarruselOpen(true);
+  }
 
   const goku = cards.find((character) => character.name === "Jiren");
 
   return (
     <div className="cards">
+      <div className="cards__header-container">
         <div className="cards__title-container">
-            <img src="../images/YourCardsES.png" 
-                    alt="Your Cards" 
-                    className="cards__image-title" />
-        </div>
+              <img src="../images/YourCardsES.png" 
+                      alt="Your Cards" 
+                      className="cards__image-title" />
+          </div>
+          <div className="cards__summon-container" onClick={handleAddCards}>
+              <img src="../images/summonIcon.png" 
+                          alt="Summon Icon" 
+                          className="cards__image-summon" />
+          </div>
+      </div>
         <div className="cards__container">
             <Card character={goku} />
 
